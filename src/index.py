0f4b8a3c90bb7e98a70e3baad2a9fe700192571b
Yuki <deadsdynasty@gmail.com>
2025-05-23 01:28:12 +0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ import os
 
 from Struct.client import Client
 
-os.system('cls')
+os.system("cls" if os.name == "nt" else "clear")
 botClient = Client()
 botClient.CreateClient()
 botClient.initClient()
